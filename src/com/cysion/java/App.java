b7f71843de485f9bc5758fade5ba1c9c292303ed
liuxshang <liuxshang@163.com>
2018-04-02 14:24:31 +0800

    重新配置项目结构

---
+++ src/com/cysion/java/App.java
@@ -0,0 +1,24 @@
+package com.cysion.java;
+
+import com.cysion.java.fresh.EasyQ;
+
+public class App {
+
+    private EasyQ easyQ;
+
+    public App() {
+        easyQ = new EasyQ();
+
+    }
+
+    public void run() {
+        System.out.println("运行..");
+//        easyQ.testReverse();
+//        easyQ.testL2U();
+//        easyQ.testfibonacci();
+//        easyQ.testRemoveElement();
+//        easyQ.testSort();
+//        easyQ.testCountNodes();
+        easyQ.testhexConversion();
+    }
+}
